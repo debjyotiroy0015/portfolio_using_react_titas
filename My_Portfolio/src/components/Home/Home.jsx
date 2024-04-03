412979b322b3c873a68d8161bfc9af858f803249
debjyotiroy <debjyotiry@gmail.com>
2024-04-03 21:13:00 +0530

    "removed react-typed"

--- My_Portfolio/src/components/Home/Home.jsx
+++ My_Portfolio/src/components/Home/Home.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import ReactTyped from "react-typed";
-import './Home.css';
-import Image1 from '../../Images/Home_Images/img1.jpg';
-import Image2 from '../../Images/Home_Images/img2.jpg';
-import Image3 from '../../Images/Home_Images/img3.jpg';
-import Resume from '../../Images/Resume_Titas Saha.pdf';
+import "./Home.css";
+import Image1 from "../../Images/Home_Images/img1.jpg";
+import Image2 from "../../Images/Home_Images/img2.jpg";
+import Image3 from "../../Images/Home_Images/img3.jpg";
+import Resume from "../../Images/Resume_Titas Saha.pdf";
 const Home = () => {
   return (
     <div className="homeintro">
       <div className="h-left">
         <span>Hello! I Am</span>
         <span>Titas Saha</span>
-        <div className="t-writter">
+        {/* <div className="t-writter">
           <ReactTyped strings={["Frontend Developement with Entry Level of Experience in Web Development", "Core Java with Intermediate Level of Experience in Coding","UI/UX Designing with Entry Level of Experience in Web Designing"]} typeSpeed={40} backSpeed={20} backDelay={600} loop />
-        </div>
-        <a className="h-button" href={Resume}>Download CV</a>
+        </div> */}
+        <a className="h-button" href={Resume}>
+          Download CV
+        </a>
       </div>
       <div className="h-right">
         <img src={Image1} alt="" />

fbf17ddd6c8d146c9cc64f659184bbc5cea8e26c
debjyotiroy <debjyotiry@gmail.com>
2024-04-03 21:47:16 +0530

    "change"

--- My_Portfolio/src/App.js
+++ My_Portfolio/src/App.js
@@ -1,4 +1,4 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import LandingPage from "./components/LandingPage/Landing";
 import School from "./components/School/School";
 import College from "./components/College/College";
@@ -6,25 +6,25 @@ import Experience from "./components/Experience/Experience";
 import Contact from "./components/Contact/Contact";
 import "./App.css";
 import LocomotiveScroll from "locomotive-scroll";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
-    useEffect(() => {
-      const locomotiveScroll = new LocomotiveScroll();
-      return () => {
-        locomotiveScroll.destroy();
-      };
-    }, []);
+  useEffect(() => {
+    const locomotiveScroll = new LocomotiveScroll();
+    return () => {
+      locomotiveScroll.destroy();
+    };
+  }, []);
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/My_Portfolio" element={<LandingPage/>} />
-          <Route path="/school" element={<School/>} />
-          <Route path="/college" element={<College/>}/>
-          <Route path="/experience" element={<Experience/>}/>
-          <Route path="/contact" element={<Contact/>}/>
+          <Route path="/" element={<Navigate to="/My_Portfolio" />} />
+          <Route path="/My_Portfolio" element={<LandingPage />} />
+          <Route path="/school" element={<School />} />
+          <Route path="/college" element={<College />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
     </BrowserRouter>
